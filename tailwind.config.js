8aae33c4e1f01c1586888d5511b3fc9735aadd8f
Bartosz Gałek <bartosz.galek@allegro.com>
2024-07-11 12:47:38 +0200

    Contrast fix (#95)
    * better contrast

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./node_modules/flowbite/**/*.js",
-        './_includes/**/*.html',
-        './_layouts/**/*.html',
-        './_drafts/**/*.md',
-        './_posts/*.md',
-        './*.md',
-        './*.html',
-        './about/*.html',
+        "./_includes/**/*.html",
+        "./_layouts/**/*.html",
+        "./_drafts/**/*.md",
+        "./_posts/*.md",
+        "./*.md",
+        "./*.html",
+        "./about/*.html",
     ],
-    darkMode: 'media',
+    darkMode: "media",
     theme: {
         colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            brand: colors.teal,
-            primary: colors.orange
+            transparent: "transparent",
+            current: "currentColor",
+            primary: colors.orange,
+        },
+        extend: {
+            typography: ({ theme }) => ({
+                DEFAULT: {
+                    css: {
+                        '--tw-format-invert-body': theme('colors.gray[300]'),
+                    },
+                },
+            }),
         },
     },
     plugins: [require("flowbite-typography"), require("flowbite/plugin")],
